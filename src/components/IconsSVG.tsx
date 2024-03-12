826b0b49630fc0330c469dc2323af493e2d869ee
grChad <bgos-jesus@hotmail.com>
2024-03-11 22:47:07 -0500

    feat: semantic title names

--- src/components/IconsSVG.tsx
+++ src/components/IconsSVG.tsx
@@ -23,7 +23,7 @@ export const IconBrush: React.FC<Props> = ({ size = 20 }) => {
 export const IconFont: React.FC<Props> = ({ size = 20 }) => {
 	return (
 		<svg width={size} height={size} viewBox="0 0 448 512" fill="var(--color-surface)">
-			<title>Renombrar</title>
+			<title>tipografia o fuente</title>
 			<path d="M254 52.8C249.3 40.3 237.3 32 224 32s-25.3 8.3-30 20.8L57.8 416H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32h-1.8l18-48H303.8l18 48H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H390.2L254 52.8zM279.8 304H168.2L224 155.1 279.8 304z" />
 		</svg>
 	)
@@ -32,7 +32,7 @@ export const IconFont: React.FC<Props> = ({ size = 20 }) => {
 export const IconText: React.FC<Props> = ({ size = 20 }) => {
 	return (
 		<svg width={size} height={size} viewBox="0 0 448 512" fill="var(--color-surface)">
-			<title>curso</title>
+			<title>nombre de texto</title>
 			<path d="M64 96v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V80C0 53.5 21.5 32 48 32H224 400c26.5 0 48 21.5 48 48v48c0 17.7-14.3 32-32 32s-32-14.3-32-32V96H256l0 320h48c17.7 0 32 14.3 32 32s-14.3 32-32 32H144c-17.7 0-32-14.3-32-32s14.3-32 32-32h48l0-320H64z" />
 		</svg>
 	)
